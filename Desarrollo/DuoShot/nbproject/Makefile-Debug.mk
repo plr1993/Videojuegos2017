a2935e9fece92a1108a60062b88f1473c1a0d70a
Sergio Pérez <sergperez90@gmail.com>
2017-03-14 13:44:29 +0100

    Crear clase personaje

--- Desarrollo/DuoShot/nbproject/Makefile-Debug.mk
+++ Desarrollo/DuoShot/nbproject/Makefile-Debug.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/Personaje.o \
 	${OBJECTDIR}/main.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/duoshot: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/duoshot ${OBJECTFILES} ${LDLIBSOPTIONS} -framework sfml-window -framework sfml-graphics -framework sfml-system
+
+${OBJECTDIR}/Personaje.o: Personaje.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Personaje.o Personaje.cpp
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
